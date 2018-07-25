c3ff51ea3ff72b8f07856be06654e36264537fad
Kozachok Sergiy <senjust@gmail.com>
2018-07-25 17:48:43 +0300

    Add Sign Up form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { FooterComponent } from './footer/footer.component';
+import { LogUpFormComponent } from './log-up-form/log-up-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginFormComponent,
+    FooterComponent,
+    LogUpFormComponent
   ],
   imports: [
     BrowserModule
